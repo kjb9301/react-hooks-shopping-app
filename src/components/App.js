7c3b52c45a5ad35f26e1866517eb25ed4247635f
kjb9301 <kjb9301@naver.com>
2020-04-02 21:46:35 +0900

    init

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.scss';
+import Header from './common/Header';
+import { Route } from 'react-router-dom';
+import { MainPage, BasketPage } from 'pages';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header/>
+        <Route exact path="/" component={MainPage}/>
+        <Route path="/basket" component={BasketPage}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/main/ShoppingList/ShoppingList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ShoppingList.scss';
+import ShoppingItem from 'components/main/ShoppingItem'
+
+const ShoppingList = ({shoppingList,handleDetail}) => {
+  const listItems = shoppingList.map((listItem) => {
+    return (
+      <ShoppingItem
+        key={listItem.id}
+        listItem={listItem}
+        handleDetail={handleDetail}
+      />
+    )
+  })
+
+  return (
+    <div className="shopping-list-wrapper">
+      {listItems}
+    </div>
+  );
+};
+
+export default ShoppingList;
