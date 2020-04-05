505074e915e0ac90a986135172b270ca62e5edd5
kjb9301 <kjb9301@naver.com>
2020-04-05 21:10:21 +0900

    migration to hooks

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Section from 'components/common/Section';
-import HomeContainer from 'containers/MainPage/HomeContainer';
+// import HomeContainer from 'containers/MainPage/HomeContainer';
+import HomeContainer from './components/HomeContainer';
 
 function Home() {
   return (
