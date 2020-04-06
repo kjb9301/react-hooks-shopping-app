fc5f09b721dd6d53cd53355455d8126ae8f5d627
kjb9301 <kjb9301@naver.com>
2020-04-06 20:38:49 +0900

    republishing Template, Nav component with styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as Pages from 'pages';
-import './App.scss';
-
-import Header from './components/common/Header';
+// import './App.scss';
 
 function App() {
   return (
     <div className='wrapper'>
       <BrowserRouter>
-        <Header />
         <Switch>
           <Route exact path='/' component={Pages.Home} />
           <Route path='/basket' component={Pages.Basket} />
---
+++ src/pages/home/components/ListContainer.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import ShoppingList from 'components/main/ShoppingList';
+import img from 'images/item.png';
+
+function ListContainer() {
+  const [shoppingData, setShoppingData] = useState([]);
+
+  const setInitData = () => {
+    axios
+      .get('dummy/goods.json')
+      .then((res) => {
+        const goods = res.data.goods.map((good) => (good = { ...good, img }));
+        console.log(goods);
+        localStorage.shoppingData = JSON.stringify(goods);
+      })
+      .then(() => {
+        const initData = JSON.parse(localStorage.getItem('shoppingData'));
+        setShoppingData(initData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    if (!localStorage.shoppingData) {
+      setInitData();
+    } else {
+      const initData = JSON.parse(localStorage.getItem('shoppingData'));
+      setShoppingData(initData);
+    }
+  }, []);
+
+  return <ShoppingList shoppingList={shoppingData} />;
+}
+
+export default ListContainer;
