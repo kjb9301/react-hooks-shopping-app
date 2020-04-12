0dd02c9f8120a4feb4f2cc7f38a44ee20309d7a9
jbkang <kjb9301@github.com>
2020-04-12 19:18:03 +0900

    republishing productList page layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 import Section from 'components/common/Section';
 import Template from 'components/common/Section/Template';
 // import HomeContainer from 'containers/MainPage/HomeContainer';
 import HomeContainer from './components/HomeContainer';
 import ProdListContainer from './components/ProdListContainer';
 import DetailContainer from './components/DetailContainer';
+import ShoppingList from 'components/main/ShoppingList';
+import ShoppingDetail from 'components/main/ShoppingDetail';
 
 function Home() {
   return (
     <Template>
-      <ProdListContainer />
+      <ListWrapper>
+        <ShoppingList />
+      </ListWrapper>
+      <DetailWrapper>
+        <ShoppingDetail />
+      </DetailWrapper>
+      {/* <ProdListContainer /> */}
       {/* <DetailContainer /> */}
     </Template>
   );
 }
+
+const ListWrapper = styled.div`
+  border: 1px solid red;
+  flex: 3;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  grid-auto-rows: minmax(auto, auto);
+  overflow: hidden scroll;
+  border: 1px solid green;
+`;
+
+const DetailWrapper = styled.div`
+  border: 1px solid blue;
+  flex: 1.5;
+`;
 
 export default Home;
