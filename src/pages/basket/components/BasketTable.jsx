542dc9b7090a636769578c00c5a0da64dd6da0f3
jbkang <kjb9301@github.com>
2020-05-19 17:12:29 +0900

    fix: Fix cart item check bug

--- src/pages/basket/components/BasketTable.jsx
+++ src/pages/basket/components/BasketTable.jsx
@@ -1,17 +1,25 @@
-import React, { useState, useContext, useCallback } from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 
 import BasketTableHeader from './BasketTableHeader';
 import BasketList from './BasketList';
-import { GlobalDispatchContext } from 'contexts/ProductContext';
+import {
+  GlobalStateContext,
+  GlobalDispatchContext,
+} from 'contexts/ProductContext';
 
 function BasketTable() {
   const [allCheck, setAllCheck] = useState(true);
+  const { basketList } = useContext(GlobalStateContext);
   const dispatch = useContext(GlobalDispatchContext);
+
+  useEffect(() => {
+    const hasAllCheck = basketList.every((item) => item.checked);
+    setAllCheck(hasAllCheck);
+  }, [basketList]);
 
   const onChangeCheckAll = useCallback(
     (e) => {
-      console.log(e.target.checked);
       setAllCheck(!allCheck);
       dispatch({ type: 'ALL_CHECK', checked: e.target.checked });
     },
